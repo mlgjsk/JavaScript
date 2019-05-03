89654c0019cd9ceabd9dbf40ce3b416a0dbb4620
mlgjsk <buba_malogajski@outlook.com>
2019-05-03 13:19:06 +0200

    Intermediate algorithm scripting

---
+++ IntermediateAlgorithms/01.summ-range-of-nums.js
@@ -0,0 +1,14 @@
+/**
+ * We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+The lowest number will not always come first.
+ */
+
+function sumAll(arr) {
+	var sum = 0;
+	for (var i = Math.min(...arr); i <= Math.max(...arr); i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+sumAll([ 1, 4 ]);
